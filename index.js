00b518839a598a06a25e61b42e1f21d52c920f5f
Eduardo-Koh <76070001+Eduardo-Koh@users.noreply.github.com>
2021-08-16 09:17:28 -0500

    Front
    Front CryptoClerks

---
+++ index.js
@@ -0,0 +1,202 @@
+var app = new Vue({ 
+    el: '#app',
+    data: {
+        message: '[ CRYPTOCLERKS ]'
+    }
+});
+
+var instructions = new Vue({ 
+    el: '#instructions',
+    data: {
+        message: '10,000 Clerks looking for a job'
+    }
+});
+
+var instructions2 = new Vue({ 
+    el: '#instructions2',
+    data: {
+        message: 'Which office will the best?'
+    }
+});
+
+var instructions3 = new Vue({ 
+    el: '#instructions3',
+    data: {
+        message: 'All CryptoClerks are minted here'
+    }
+});
+
+var instructions4 = new Vue({ 
+    el: '#instructions4',
+    data: {
+        message: 'at a fixed price of .05 ETH each'
+    }
+});
+
+var instructions5 = new Vue({ 
+    el: '#instructions5',
+    data: {
+        message: 'No bonding curve, no price tiers'
+    }
+});
+
+var instructions6 = new Vue({ 
+    el: '#instructions6',
+    data: {
+        message: 'More than 200 items that will'
+    }
+});
+
+var instructions7 = new Vue({ 
+    el: '#instructions7',
+    data: {
+        message: 'help your Clerks to become Bosses'
+    }
+});
+
+var instructions8 = new Vue({ 
+    el: '#instructions8',
+    data: {
+        message: '20% SALES'
+    }
+});
+
+var instructions9 = new Vue({ 
+    el: '#instructions9',
+    data: {
+        message: 'Offices will be created, first 500 owners'
+    }
+});
+
+var instructions10 = new Vue({ 
+    el: '#instructions10',
+    data: {
+        message: 'will get one so they can put their Clerks'
+    }
+});
+
+var instructions11 = new Vue({ 
+    el: '#instructions11',
+    data: {
+        message: 'to work in our future game'
+    }
+});
+
+var instructions12 = new Vue({ 
+    el: '#instructions12',
+    data: {
+        message: '40% SALES'
+    }
+});
+
+var instructions13 = new Vue({ 
+    el: '#instructions13',
+    data: {
+        message: "Manager unlock and reveal, 1% drop rate,"
+    }
+});
+
+var instructions14 = new Vue({ 
+    el: '#instructions14',
+    data: {
+        message: "they'll work harder, better and will yield"
+    }
+});
+
+var instructions15 = new Vue({ 
+    el: '#instructions15',
+    data: {
+        message: 'more clerkcoins'
+    }
+});
+
+var instructions16 = new Vue({ 
+    el: '#instructions16',
+    data: {
+        message: '60% SALES'
+    }
+});
+
+var instructions17 = new Vue({ 
+    el: '#instructions17',
+    data: {
+        message: "Item cards will be created, you'll be able"
+    }
+});
+
+var instructions18 = new Vue({ 
+    el: '#instructions18',
+    data: {
+        message: "to preview it's rarity, holders will get one"
+    }
+});
+
+var instructions19 = new Vue({ 
+    el: '#instructions19',
+    data: {
+        message: 'extra item for reworking your clerks'
+    }
+});
+
+var instructions20 = new Vue({ 
+    el: '#instructions20',
+    data: {
+        message: '80% SALES'
+    }
+});
+
+var instructions121 = new Vue({ 
+    el: '#instructions21',
+    data: {
+        message: "CEO unlock and reveal, 5% drop rate,"
+    }
+});
+
+var instructions22 = new Vue({ 
+    el: '#instructions22',
+    data: {
+        message: "he won't work, but he'll take your team"
+    }
+});
+
+var instructions123 = new Vue({ 
+    el: '#instructions23',
+    data: {
+        message: "to the moon"
+    }
+});
+
+var instructions24 = new Vue({ 
+    el: '#instructions24',
+    data: {
+        message: '100% SALES'
+    }
+});
+
+var instructions125 = new Vue({ 
+    el: '#instructions25',
+    data: {
+        message: "20 empty chairs, so you can build your"
+    }
+});
+
+var instructions26 = new Vue({ 
+    el: '#instructions26',
+    data: {
+        message: "clerk from 0, with an item pack;a set"
+    }
+});
+
+var instructions127 = new Vue({ 
+    el: '#instructions27',
+    data: {
+        message: "of 10 items ranging from rare to mithyc"
+    }
+});
+
+var instructions128 = new Vue({ 
+    el: '#instructions28',
+    data: {
+        message: "Item Pack 1st Drop Presale will begin"
+    }
+});
